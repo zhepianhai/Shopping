2db6e1a3ec573b961ca45a58aa7880abb6b385db
adminZPH <1364778591@qq.com>
2017-04-11 16:18:35 +0800

    ui

--- app/src/main/java/zph/zhjx/com/shopping/ui/MainActivity.java
+++ app/src/main/java/zph/zhjx/com/shopping/ui/MainActivity.java
@@ -1,7 +1,9 @@
-package zph.zhjx.com.shopping;
+package zph.zhjx.com.shopping.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import zph.zhjx.com.shopping.R;
 
 public class MainActivity extends AppCompatActivity {
 
